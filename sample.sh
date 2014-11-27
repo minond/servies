4cd9a85176d1b850e916cbbfc660314a05e31c6e
Marcos Minond <minond.marcos@gmail.com>
2014-11-26 22:01:00 -0700

    using patterns to route request

--- sample.sh
+++ sample.sh
@@ -2,6 +2,7 @@ get "/" echo "welcome!!"
 get "/ps" ps aux
 get "/netstat" netstat -pan
 
+# for testing
 get "/get" echo "get response"
 post "/post" echo "post response"
 put "/put" echo "put response"
--- test/patterns.sh
+++ test/patterns.sh
@@ -1,13 +1,22 @@
 source ./servies
 
-expect "slashes to be optional" \
-    $(test "$(url_pattern "/welcome/")" = "/\?welcome/\?")
+expect "slashes to be optional in extractors" \
+    $(test "$(url_extractor "/welcome/")" = "s/\/{0,}welcome\/{0,}//")
 
-expect "groups are replaced with regular expressions" \
-    $(test "$(url_pattern "/welcome/:name")" = "/\?welcome/\?\w\+")
+expect "groups are replaced with regular expressions in extractors" \
+    $(test "$(url_extractor "/welcome/:name")" = "s/\/{0,}welcome\/{0,}(\w+)/ \1/")
 
-expect "optional groups are handles correctly" \
-    $(test "$(url_pattern "/welcome/:name?")" = "/\?welcome/\?\w\+\?")
+expect "optional groups are handles correctly in extractors" \
+    $(test "$(url_extractor "/welcome/:name?")" = "s/\/{0,}welcome\/{0,}(\w+){0,}/ \1/")
+
+expect "slashes to be optional in patterns" \
+    $(test "$(url_pattern "/welcome/")" = "^/\?welcome/\?\$")
+
+expect "groups are replaced with regular expressions in patterns" \
+    $(test "$(url_pattern "/welcome/:name")" = "^/\?welcome/\?\w\+\$")
+
+expect "optional groups are handles correctly in patterns" \
+    $(test "$(url_pattern "/welcome/:name?")" = "^/\?welcome/\?\w\+\?\$")
 
 expect "single group to be found" \
     $(test "$(url_groups "/welcome/:name")" = "name")
